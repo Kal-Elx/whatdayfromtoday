807311fbf1c1c49a0c9e6abf6b7fa30dd0b877cb
Kalle Johansson <kallej1johansson@gmail.com>
2024-05-19 20:11:54 +0200

    feat: title

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css';
 import Calculator from './components/Calculator/Calculator';
+import Title from './components/Title/Title';
 
 const App = () => {
+  const today = Date.now();
+
   return (
-    <div
-      style={{
-        width: 500,
-        height: 500,
-      }}
-    >
-      <Calculator
-        onNumberTap={(number) => console.log(number)}
-        onOperatorTap={() => console.log('Operator')}
-        onDeleteTap={() => console.log('Delete')}
-        onResultTap={() => console.log('Result')}
-      />
+    <div className="app-layout">
+      <Title days={0} isNegative={false} inputDate={today} resultDate={today} />
+      <div
+        style={{
+          width: 500,
+          height: 500,
+        }}
+      >
+        <Calculator
+          onNumberTap={(number) => console.log(number)}
+          onOperatorTap={() => console.log('Operator')}
+          onDeleteTap={() => console.log('Delete')}
+          onResultTap={() => console.log('Result')}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,31 @@
+import './Title.css';
+
+const Title = ({ days, isNegative, inputDate, resultDate }) => {
+  return (
+    <div className="title">
+      <span className="day-input">{`${isNegative ? -days : days}`}</span>
+      {' Days from '}
+      <span className="input-date">{`${formatDate(inputDate, false)}`}</span>
+      {' is '}
+      <span className="result-date">{`${formatDate(resultDate, true)}`}</span>
+    </div>
+  );
+};
+
+const formatDate = (date, showWeekday) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  if (showWeekday) {
+    options.weekday = 'long';
+  }
+
+  return new Date(date)
+    .toLocaleDateString('en-US', options)
+    .replaceAll(',', '');
+};
+
+export default Title;
