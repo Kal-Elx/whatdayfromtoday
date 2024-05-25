765f8eccc692df6877af74acc9f9283265434aa1
Kalle Johansson <kallej1johansson@gmail.com>
2024-05-25 10:56:29 +0200

    feat: relative days

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -12,17 +12,28 @@ const Title = ({ days, inputDate, resultDate, onDateSelected }) => {
         child={
           <span className="input-date">{`${formatDate(
             inputDate,
-            false
+            false,
+            true
           )}`}</span>
         }
       />
       {' is '}
-      <span className="result-date">{`${formatDate(resultDate, true)}`}</span>
+      <span className="result-date">{`${formatDate(resultDate)}`}</span>
     </div>
   );
 };
 
-const formatDate = (date, showWeekday) => {
+const formatDate = (date, showWeekday = true, showRelativeDay = false) => {
+  if (showRelativeDay) {
+    if (isYesterday(date)) {
+      return 'YESTERDAY';
+    } else if (isToday(date)) {
+      return 'TODAY';
+    } else if (isTomorrow(date)) {
+      return 'TOMORROW';
+    }
+  }
+
   const options = {
     year: 'numeric',
     month: 'long',
@@ -36,6 +47,24 @@ const formatDate = (date, showWeekday) => {
   return new Date(date)
     .toLocaleDateString('en-US', options)
     .replaceAll(',', '');
+};
+
+const isYesterday = (date) => dateDiffInDays(date, Date.now()) == 1;
+
+const isToday = (date) => dateDiffInDays(date, Date.now()) == 0;
+
+const isTomorrow = (date) => dateDiffInDays(date, Date.now()) == -1;
+
+const dateDiffInDays = (startDate, endDate) => {
+  const a = new Date(startDate);
+  const b = new Date(endDate);
+
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
 };
 
 export default Title;
