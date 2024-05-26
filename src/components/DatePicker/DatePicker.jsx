a48fc80075d39c4268b6be9216f1f625dc795372
Kalle Johansson <kallej1johansson@gmail.com>
2024-05-26 21:34:22 +0200

    feat: style date picker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,9 +1,9 @@
 import ReactDatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.css';
 
 const DatePicker = ({ date, onDateSelected, child }) => {
   return (
-    <div className="date-picker">
+    <div>
       <ReactDatePicker
         selected={date}
         onChange={onDateSelected}
