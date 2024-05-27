b169a679c1d18d9e54dec41121f57987546eb1e4
Kalle Johansson <kallej1johansson@gmail.com>
2024-05-27 22:37:03 +0200

    feat: set inital days to 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Calculator from './components/Calculator/Calculator';
 import Title from './components/Title/Title';
 
 const App = () => {
-  const [days, setDays] = useState(0);
+  const [days, setDays] = useState(1);
   const [inputDate, setInputDate] = useState(Date.now());
-  const [resultDate, setResultDate] = useState(Date.now());
+  const [resultDate, setResultDate] = useState(addDays(Date.now(), days));
 
   return (
     <div className="app-layout">
@@ -26,14 +26,16 @@ const App = () => {
         }
         onOperatorTap={() => setDays(-days)}
         onDeleteTap={() => setDays(parseInt(days.toString().slice(0, -1)) || 0)}
-        onResultTap={() => {
-          const result = new Date(inputDate);
-          result.setDate(result.getDate() + days);
-          return setResultDate(result);
-        }}
+        onResultTap={() => setResultDate(addDays(inputDate, days))}
       />
     </div>
   );
+};
+
+const addDays = (date, days) => {
+  const result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 };
 
 export default App;
